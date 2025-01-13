dd64e5294809f75d071fa0a8f5e00992fa089b9c
ahh0619 <ahh900619@gmail.com>
2025-01-13 13:40:55 +0900

    🐛 Bug : 프로필 수정 후 리렌더링 안되는부분 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,20 +3,11 @@ import { getUserProfile } from '@/utils/mypage/action';
 import MyPageComponent from './_components/MyPageComponent';
 
 const MyPage = async () => {
-  // 서버에서 사용자 프로필 데이터를 가져옵니다.
   const userProfile = await getUserProfile();
-
-  if (!userProfile) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <p>유저 정보를 가져올 수 없습니다.</p>
-      </div>
-    );
-  }
 
   return (
     <div className="mx-auto flex min-h-screen max-w-md flex-col items-center">
-      <MyPageComponent userProfile={userProfile} />
+      <MyPageComponent initialUserProfile={userProfile} />
     </div>
   );
 };
