4300e8b50dd7473eb62eb1a4ce6613f8c1d7fc90
ahh0619 <ahh900619@gmail.com>
2025-01-13 16:23:45 +0900

    ✨ Feat : 공통 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import MockSection from '@/components/common/MockSection';
+
 const Home = () => {
   return (
     <>
-      <div className="flex items-center bg-red-500 p-4 text-center hover:bg-blue-500"></div>
+      <MockSection />
     </>
   );
 };
