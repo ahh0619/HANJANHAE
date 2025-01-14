081f7ee0f04737085dfffd80a085bbe9525a0600
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-01-15 01:56:04 +0900

    ♻️ Refactor: Page 컴포넌트를 서버 컴포넌트로 변경하고 PreferencesForm 컴포넌트 분리
    Page 컴포넌트를 서버 컴포넌트로 변경하고 PreferencesForm 컴포넌트 분리

--- src/app/preferences/customization/page.tsx
+++ src/app/preferences/customization/page.tsx
@@ -1,29 +1,8 @@
-'use client';
-
 import BackButton from '@/components/common/BackButton';
-import usePreferences from '@/hooks/preference/usePreferences';
 
-import AlcoholLevelSelector from './_components/AlcoholLevelSelector';
-import AlcoholTypeSelector from './_components/AlcoholTypeSelector';
-import FavoriteFoodInput from './_components/FavoriteFoodInput';
-import TasteSelector from './_components/TasteSelector';
+import PreferencesForm from './_components/PreferencesForm';
 
 const Page = () => {
-  const {
-    preferences,
-    handlePreferenceChange,
-    handleTypeChange,
-    handleFoodChange,
-    handleSubmit,
-    isFormComplete,
-    hasPreferencesChanged,
-    isLoading,
-    error,
-  } = usePreferences();
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
   return (
     <div className="mx-auto max-w-lg p-6">
       <div className="relative mb-5 flex w-full items-center">
@@ -33,34 +12,7 @@ const Page = () => {
         <h1 className="mx-auto text-xl font-bold">내 취향 조사</h1>
       </div>
 
-      <AlcoholTypeSelector
-        preferences={preferences}
-        handleTypeChange={handleTypeChange}
-      />
-      <AlcoholLevelSelector
-        preferences={preferences}
-        handleSelect={handlePreferenceChange}
-      />
-      <TasteSelector
-        preferences={preferences}
-        handleSelect={handlePreferenceChange}
-      />
-      <FavoriteFoodInput
-        preferences={preferences}
-        handleFoodChange={handleFoodChange}
-      />
-
-      <button
-        onClick={handleSubmit}
-        disabled={!isFormComplete || !hasPreferencesChanged}
-        className={`mb-10 w-full rounded-md py-3 text-lg font-bold ${
-          isFormComplete && hasPreferencesChanged
-            ? 'bg-black text-white'
-            : 'cursor-not-allowed bg-gray-300 text-gray-500'
-        }`}
-      >
-        저장하기
-      </button>
+      <PreferencesForm />
     </div>
   );
 };
