e270e0a32c9682c23425c5bde480f0c3412afe40
ParkGana <gnchoco97@gmail.com>
2025-01-15 16:53:47 +0900

    ✨ Feat : 카카오 지도 페이지로 이동하는 구현
    다이닝바 상세 페이지에서 지도의 마커 클릭 시 카카오 지도 페이지로 이동

--- src/app/place/_components/KakaoMap.tsx
+++ src/app/place/_components/KakaoMap.tsx
@@ -6,6 +6,11 @@ type PlaceMapProps = {
 };
 
 const KakaoMap = ({ location_x, location_y }: PlaceMapProps) => {
+  const handleMoveToMap = () => {
+    const kakaoMapUrl = `https://map.kakao.com/link/map/${location_x},${location_y}`;
+    window.open(kakaoMapUrl, '_blank');
+  };
+
   return (
     <div className="px-2">
       <Map
@@ -13,7 +18,10 @@ const KakaoMap = ({ location_x, location_y }: PlaceMapProps) => {
         center={{ lat: location_x, lng: location_y }}
         level={3}
       >
-        <MapMarker position={{ lat: location_x, lng: location_y }} />
+        <MapMarker
+          position={{ lat: location_x, lng: location_y }}
+          onClick={handleMoveToMap}
+        />
       </Map>
     </div>
   );
