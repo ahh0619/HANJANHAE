7d2dac35248764153e9a8f4040461d5d1b19d784
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-01-17 23:38:38 +0900

    ✨ Feat: 좋아요 데이터가 없을 경우 메시지와 이미지를 추가하고, 좋아요 페이지, 카드 스타일 개선
    좋아요 데이터가 없을 경우 메시지와 이미지를 추가하고, 카드, 페이지 스타일을 개선

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -8,6 +8,10 @@ type ProductCardProps = {
   name: string;
   imageUrl: string;
   userId: string | null;
+  width?: string;
+  height?: string;
+  marginBottom?: string;
+  imgHeight?: string | number;
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({
@@ -15,11 +19,22 @@ const ProductCard: React.FC<ProductCardProps> = ({
   name,
   imageUrl,
   userId,
+  width = '124px',
+  height = '186px',
+  marginBottom = '0px',
+  imgHeight = '152px',
 }) => {
   return (
-    <div className="relative flex w-32 flex-col">
+    <div
+      className="relative flex flex-col"
+      style={{
+        width,
+        height,
+        marginBottom,
+      }}
+    >
       {/* 좋아요 버튼 */}
-      <div className="absolute bottom-6 right-0 z-10">
+      <div className="absolute bottom-[30px] right-0 z-10">
         <LikeButton drinkId={id} userId={userId} />
       </div>
 
@@ -28,7 +43,10 @@ const ProductCard: React.FC<ProductCardProps> = ({
         className="flex flex-col"
       >
         {/* 이미지 */}
-        <div className="relative aspect-[4/5] w-full overflow-hidden rounded-lg bg-gray-100 bg-opacity-50">
+        <div
+          className="relative aspect-[4/5] w-full overflow-hidden rounded-lg border border-grayscale-200 bg-gray-100 bg-opacity-50"
+          style={{ height: imgHeight }}
+        >
           <img
             src={imageUrl}
             alt={name}
@@ -36,7 +54,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
           />
         </div>
         {/* 이름 */}
-        <div className="mt-2 w-full overflow-hidden text-ellipsis whitespace-nowrap text-left text-title-mm">
+        <div className="mt-3 w-full overflow-hidden text-ellipsis whitespace-nowrap text-left text-title-mm">
           {name}
         </div>
       </Link>
