ba03eec807f1c11ff8ba67c01738ea0ca48118f5
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-01-20 02:34:26 +0900

    ✨ Feat: 좋아요 페이지 보호 로직 추가 및 조건문 통합
    좋아요 페이지 보호 로직 추가 및 조건문 통합

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -3,6 +3,7 @@ import { NextResponse, type NextRequest } from 'next/server';
 
 export const updateSession = async (request: NextRequest) => {
   const supabaseResponse = NextResponse.next({ request });
+  const protectedRoutes = ['/mypage', '/like'];
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -30,8 +31,8 @@ export const updateSession = async (request: NextRequest) => {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  // 로그인하지 않은 사용자가 마이 페이지에 접근하려고 하는 경우 로그인 페이지로 이동
-  if (!user && ['/mypage'].includes(request.nextUrl.pathname)) {
+  // 로그인하지 않은 사용자가 마이 페이지나 좋아요 페이지에 접근하려고 하는 경우 로그인 페이지로 이동
+  if (!user && protectedRoutes.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/signin', request.url));
   }
 
