74230cef10da5d6e23497594c787d8c5434ef231
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-01-20 01:42:29 +0900

    ✨ Feat: 그만할래요 모달 추가, 모달 스타일 개선
    그만할래요 모달 추가, 모달 스타일 개선

--- src/app/survey/_components/StepButton.tsx
+++ src/app/survey/_components/StepButton.tsx
@@ -1,3 +1,8 @@
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import Modal from '@/components/common/Modal';
+
 type StepButtonProps = {
   content: string;
   onClick: () => void;
@@ -9,6 +14,17 @@ const StepButton = ({
   onClick,
   disabled = false,
 }: StepButtonProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="fixed bottom-[60px] left-0 flex w-full flex-col items-center px-[20px]">
       <button
@@ -22,9 +38,34 @@ const StepButton = ({
       >
         {content}
       </button>
-      <button className="mb-[20px] mt-[24px] h-[48px] text-label-lm text-grayscale-500 underline">
+
+      <button
+        className="mb-[20px] mt-[24px] h-[48px] text-label-lm text-grayscale-500 underline"
+        onClick={openModal}
+      >
         그만할래요
       </button>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title="취향 설문을 종료하시겠어요?"
+        content={`지금 종료하시면 설문은 저장되지 않습니다.`}
+        secondaryAction={{
+          text: '계속하기 ',
+          onClick: () => {
+            closeModal();
+          },
+        }}
+        primaryAction={{
+          text: '종료하기',
+          onClick: () => {
+            router.push('/');
+            closeModal();
+          },
+        }}
+        showCloseButton={false}
+      />
     </div>
   );
 };
