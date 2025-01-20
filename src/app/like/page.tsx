a338f4249c8d45150f1a3e018874dc705fe506aa
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-01-21 01:35:39 +0900

    💄 Style: 좋아요 페이지 width 개선
    좋아요 페이지 width, 정렬 개선

--- src/app/like/page.tsx
+++ src/app/like/page.tsx
@@ -2,7 +2,7 @@ import LikesContent from './_components/LikesContent';
 
 const Page = () => {
   return (
-    <div>
+    <div className="mx-auto flex min-h-screen max-w-md flex-col items-center">
       <div className="mb-[32px] flex h-[44px] w-full items-center justify-center px-2">
         <h1 className="text-title-xl leading-[44px]">좋아요</h1>
       </div>
