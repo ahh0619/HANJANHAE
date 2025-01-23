c0ca7c56e4dfa7bd7ed38337ea09a957d161b1f4
ParkGana <gnchoco97@gmail.com>
2025-01-23 19:28:12 +0900

    ♻️ Refactor : 카카오맵 API 관련 script 호출 위치 변경
    카카오맵 API 관련 script를 전역이 아닌 사용 페이지에서 호출함으로써 불필요한 API 호출 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,6 @@ const RootLayout = ({
           src="https://developers.kakao.com/sdk/js/kakao.min.js"
           strategy="lazyOnload"
         />
-        <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
-          strategy="beforeInteractive"
-        />
       </head>
       <body className="antialiased">
         <Providers>
